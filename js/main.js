24115316afdf438e69c2fc13f6af0a341928c509
Dejorden <d.moerman@hotmail.nl>
2023-06-05 20:24:26 +0200

    add focus function for cards

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ for (let i = 0; i < profiles.length; i++) {
         profiles[i].children[1].children[0].style.animationName = "popup";
         profiles[i].children[2].children[0].style.animationName = "popupHeart";
     }
+    profiles[i].onkeyup = function (event) {
+        if (event.keyCode === 32) {
+            profiles[i].style.animationName = "expand";
+            profiles[i].children[1].children[0].style.animationName = "popup";
+            profiles[i].children[2].children[0].style.animationName = "popupHeart";
+        }
+    }
 }
